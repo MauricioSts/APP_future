0bdcdd2776b34969315d8ba5263a4cbbd60a4ff4
MauricioSts <mauricio.santos.110@ufrn.edu.br>
2025-03-22 12:57:16 -0300

    future

---
+++ android/app/src/main/kotlin/com/example/app_consumo_servico_avancado/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_consumo_servico_avancado
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:app_consumo_servico_avancado/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Home()));
+}
